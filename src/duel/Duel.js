77463766b7b3efe30bebe86d9d999c7f52da0122
White Dog <dlrlgus1204@naver.com>
2021-11-15 23:00:57 +0900

    duel rotation 2021 update

--- src/duel/Duel.js
+++ src/duel/Duel.js
@@ -22,37 +22,30 @@ function Duel() {
 
     let [timer, setTimer] = useState('다음 아레나까지 ' + (tt - hour) + '시간 ' + (59 - minute) + '분');
 
-    if ( month === 1 || month === 2 || month === 3 || month === 6 || month === 7 || month === 9 || month === 10) {
-        if (day % 2 === 0) {
-            // 짝수일
-            if(hour < 10){
+    if (month === 11 || month === 12) {
+        if (day % 3 === 0) {
+            // 현재 심야
+            // 전날 석양
+            if (hour < 10) {
                 dayCheck = 1;
             } else {
-            dayCheck = 0;
+                dayCheck = 2;
             }
-        } else if (day % 2 === 1) {
-            // 홀수일
-            if(hour < 10){
+        } else if (-1 === (day % 3) - 1) {
+            // 현재 석양
+            // 전날 낮
+            if (hour < 10) {
                 dayCheck = 0;
             } else {
-            dayCheck = 1;
-            }
-        }
-    }
-    if ( month === 4 || month === 5 || month === 8 || month === 11 || month === 12) {
-        if (day % 2 === 0) {
-            // 짝수일
-            if(hour < 10){
-                dayCheck = 0;
-            } else {
-            dayCheck = 1;
-            }
-        } else if (day % 2 === 1) {
-            // 홀수일
-            if(hour < 10){
                 dayCheck = 1;
+            }
+        } else {
+            // 현재 낮
+            // 전날 심야
+            if (hour < 10) {
+                dayCheck = 2;
             } else {
-            dayCheck = 0;
+                dayCheck = 0;
             }
         }
     }
@@ -72,9 +65,23 @@ function Duel() {
                 DayCheck();
                 return setTimer('다음 아레나까지 ' + (tt - hour) + '시간 ' + (59 - minute) + '분');
             });
-        } else {
+        } else if (dayCheck === 0) {
             let sync2 = document.querySelector('.afternoonFooter svg');
             sync2.addEventListener('click', function () {
+                today = new Date();
+                hour = today.getHours();
+                minute = today.getMinutes();
+                if (hour < 10) {
+                    tt = 9;
+                } else {
+                    tt = 33;
+                }
+                DayCheck();
+                return setTimer('다음 아레나까지 ' + (tt - hour) + '시간 ' + (59 - minute) + '분');
+            });
+        } else {
+            let sync3 = document.querySelector('.nightFooter svg');
+            sync3.addEventListener('click', function () {
                 today = new Date();
                 hour = today.getHours();
                 minute = today.getMinutes();
@@ -89,7 +96,7 @@ function Duel() {
         }
     }, []);
 
-
+    // 2이면 심야
     // 1이면 석양
     // 0이면 낮
     return (
@@ -113,24 +120,44 @@ function Duel() {
                     </div>
                     <img src='./imgs/duel_map_sunset.jpg' className='sunsetImg'></img>
                 </section>
-                : <section className='duel'>
-                    <div className='imgBox'>
-                        <div>
-                            <div className='allBox'>
-                                <div className='afternoonBox'>
-                                    <h1>결투</h1>
-                                    <span>라푼젤의 무너진 탑 - 낮</span>
-                                    <img src='./imgs/duel_icon.png'></img>
-                                </div>
-                                <img src='./imgs/duel_minimap.jpg'></img>
-                                <div className='afternoonFooter'>
-                                    <h2><FontAwesomeIcon icon={faSync} /> {timer}</h2>
+                : (dayCheck === 0 ?
+                    <section className='duel'>
+                        <div className='imgBox'>
+                            <div>
+                                <div className='allBox'>
+                                    <div className='afternoonBox'>
+                                        <h1>결투</h1>
+                                        <span>라푼젤의 무너진 탑 - 낮</span>
+                                        <img src='./imgs/duel_icon.png'></img>
+                                    </div>
+                                    <img src='./imgs/duel_minimap.jpg'></img>
+                                    <div className='afternoonFooter'>
+                                        <h2><FontAwesomeIcon icon={faSync} /> {timer}</h2>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    </div>
-                    <img src='./imgs/duel_map_afternoon.jpg' className='afternoonImg'></img>
-                </section>
+                        <img src='./imgs/duel_map_afternoon.jpg' className='afternoonImg'></img>
+                    </section>
+                    : <section className='duel'>
+                        <div className='imgBox'>
+                            <div>
+                                <div className='allBox'>
+                                    <div className='nightBox'>
+                                        <h1>결투</h1>
+                                        <span>라푼젤의 무너진 탑 - 심야</span>
+                                        <img src='./imgs/duel_icon.png'></img>
+                                    </div>
+                                    <img src='./imgs/duel_minimap_night.jpg'></img>
+                                    <div className='nightFooter'>
+                                        <h2><FontAwesomeIcon icon={faSync} /> {timer}</h2>
+                                    </div>
+                                </div>
+                            </div>
+                        </div>
+                        <img src='./imgs/duel_map_night.jpg' className='nightImg'></img>
+                    </section>
+                )
             }
         </>
     )
@@ -143,40 +170,30 @@ function DayCheck() {
     let day = today.getDate();
     let dayCheck = 0;
 
-    if ( month === 1 || month === 2 || month === 3 || month === 6 || month === 7 || month === 9 || month === 10) {
-        if (day % 2 === 0) {
-            // 짝수일
-            // 낮 맵이 나와야함
-            if(hour < 10){
+    if (month === 11 || month === 12) {
+        if (day % 3 === 0) {
+            // 현재 심야
+            // 전날 석양
+            if (hour < 10) {
                 dayCheck = 1;
             } else {
-            dayCheck = 0;
+                dayCheck = 2;
             }
-        } else if (day % 2 === 1) {
-            // 홀수일
-            if(hour < 10){
+        } else if (-1 === (day % 3) - 1) {
+            // 현재 석양
+            // 전날 낮
+            if (hour < 10) {
                 dayCheck = 0;
             } else {
-            dayCheck = 1;
-            }
-        }
-    }
-    if ( month === 4 || month === 5 || month === 8 || month === 11 || month === 12) {
-        if (day % 2 === 0) {
-            // 짝수일
-            // 석양
-            if(hour < 10){
-                dayCheck = 0;
-            } else {
-            dayCheck = 1;
-            }
-        } else if (day % 2 === 1) {
-            // 홀수일
-            // 낮
-            if(hour < 10){
                 dayCheck = 1;
+            }
+        } else {
+            // 현재 낮
+            // 전날 심야
+            if (hour < 10) {
+                dayCheck = 2;
             } else {
-            dayCheck = 0;
+                dayCheck = 0;
             }
         }
     }
